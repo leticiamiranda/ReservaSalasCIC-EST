3351e6a2b138ec47b402b4ba255847f39e0a4541
Lucas Avelino <130013072@aluno.unb.br>
2016-12-11 20:40:55 +0000

    [Delivers #134988507] Tentando resolver problema das credenciais

--- app/models/reserva.rb
+++ app/models/reserva.rb
@@ -10,7 +10,7 @@ class Reserva < ActiveRecord::Base
     @@OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
     @@APPLICATION_NAME = 'Reserva de Salas CIC/EST'
     @@CLIENT_SECRETS_PATH = File.absolute_path('./') + '/config/client_secret.json'
-    @@CREDENTIALS_PATH = File.join(Dir.home, '.credentials',
+    @@CREDENTIALS_PATH = File.join(Dir.pwd, '.credentials',
                                  "reservasalascicest.yaml")
     @@SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR
     
@@ -30,16 +30,6 @@ class Reserva < ActiveRecord::Base
         client_id, @@SCOPE, token_store)
       user_id = 'default'
       credentials = authorizer.get_credentials(user_id)
-      if credentials.nil?
-        url = authorizer.get_authorization_url(
-          base_url: @@OOB_URI)
-        puts "Open the following URL in the browser and enter the " +
-             "resulting code after authorization"
-        puts url
-        code = gets
-        credentials = authorizer.get_and_store_credentials_from_code(
-          user_id: user_id, code: code, base_url: @@OOB_URI)
-      end
       credentials
     end
     

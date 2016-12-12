03d77715e4bc58daba99c4df02487d5c9c7a5dcc
letiribeiro <120125170@aluno.unb.br>
2016-12-12 19:36:50 +0000

    testes de controller para usuario

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,15 +2,19 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe UsersController do
-    describe 'testes para user/sign_in' do
-        before :each do
-          @request.env["devise.mapping"] = Devise.mappings[:users]
-          user = FactoryGirl.create(:users)
-          user.confirm!
-          sign_in user
-        end
-        it "should have a current_user" do
-          expect(subject.current_user).to_not eq(nil)
-        end
-    end
+  describe 'testes para user/sign_in' do
+      before :each do
+        @request.env["devise.mapping"] = Devise.mappings[:users]
+        user = FactoryGirl.create(:users)
+        user.confirm!
+        sign_in user
+      end
+      it "deve ter usuario atual (current_user) mesmo que nao haja ninguem logado" do
+        expect(subject.current_user).to_not eq(nil)
+      end
+      it "should get index" do
+        get 'index'
+        expect(response).to be_success
+      end
+  end
 end

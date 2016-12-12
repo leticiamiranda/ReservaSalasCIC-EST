21c31b2d3e96ef803acc7508028bc9b83f0b0d5b
Victória Goularte <victoriagr@aluno.unb.br>
2016-12-12 19:16:17 -0200

    Novos testes para cadastrar salas.

--- features/cadastrar_salas.feature
+++ features/cadastrar_salas.feature
@@ -8,21 +8,17 @@ Feature: Add sala
 
     Scenario: Add Sala, but have no permission
 		Given My role is teacher
+		And I am logged in
 		When I am on the salas page
 		And I follow "Cadastrar sala"
 		Then I see a alert message of access denied
 
 	Scenario: Add Sala, with permission
-		
+		Given My role is admin
+		And I am logged in
 		When I am on the salas page
 		And I follow "Cadastrar sala"
 		Then I am on the cadastrar sala page
 		When fill the form
 		Then I Should see my register in the list
 
-	Scenario: Try to add sala, but do not fill all fields	
-		Given My role is admin
-		Given I am on the cadastrar sala page
-		When I fill in "Qtd lugares" with "10" 
-		And I fill in "Disponivel" with "True"
-		Then I should see a info message
--- features/step_definitions/cadastrar_sala_steps.rb
+++ features/step_definitions/cadastrar_sala_steps.rb
@@ -10,10 +10,10 @@ end
 
 When(/^fill the form$/) do
 	expect(page).to have_content "Nome"
-	fill_in 'nome', :with => "LARA"
-	fill_in 'Qtd lugares', :with => "20"
-	fill_in 'Disponivel', :with => "True"
-	click_button "Cadastrar sala"
+	fill_in 'sala_nome', :with => "LARA"
+	fill_in 'sala_qtd_lugares', :with => "20"
+	
+	click_button "Cadastrar"
 end
 
 Then(/^I Should see my register in the list$/) do
@@ -21,9 +21,11 @@ Then(/^I Should see my register in the list$/) do
 end
 
 Then (/^I should see a info message$/) do
-  page.should have_selector ".info", text: "Preecha este campo."
+  expect(page).to have_content "Preencha este campo."
 end
 
 Then (/^I see a alert message of access denied$/) do
 	expect(page).to have_selector ".alert", text: "Você não tem permissão para acessar essa página."
 end
+
+

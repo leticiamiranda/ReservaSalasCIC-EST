f8886f04df17aa1d3a4b55c7ece5b69f2b8f7374
letiribeiro <120125170@aluno.unb.br>
2017-07-11 20:35:05 +0000

    adding rspec tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,11 +8,42 @@ describe UsersController do
         user = FactoryGirl.create(:user)
         sign_in user
       end
+      it "deve ter usuario atual (current_user)" do
+        expect(subject.current_user).to_not eq(nil)
+      end
+      it "nao deve funcionar o :get index para usuario normal" do
+        get :index
+        expect(response).not_to be_success
+      end
+      it "nao deve funcionar o :get show para usuario normal" do
+        get :show
+        expect(response).not_to be_success
+      end
+      it "nao deve funcionar o :get edit para usuario normal" do
+        get :edit
+        expect(response).not_to be_success
+      end
+      
+  end
+  describe 'testes para admin' do
+    before :each do
+        @request.env["devise.mapping"] = Devise.mappings[:users]
+        user = FactoryGirl.create(:user, :admin)
+        sign_in user
+      end
+      it "deve funcionar o :get index para usuario admin" do
+        get :index
+        expect(response).to be_success
+      end
       it "deve ter usuario atual (current_user) mesmo que nao haja ninguem logado" do
         expect(subject.current_user).to_not eq(nil)
       end
-      it "deve funcionar o :get index" do
-        get :index
+      it "deve funcionar o :get show para usuario admin" do
+        get :show
+        expect(response).to be_success
+      end
+      it "deve funcionar o :get edit para usuario admin" do
+        get :edit
         expect(response).to be_success
       end
   end
